1d222969253cfa47689c139e4443d5a98267b0a0
James W <timeimp@icloud.com>
2015-04-26 13:52:11 +1000

    Removed requirement for DatabaseAdaptor to have u+p passed to it.
    Previously, you had to pass the SQL username and password. Now you
    simply extend this class.
    
    You need to require_once 'db.config.php' BEFORE calling this file,
    otherwise the SQL query to connect will fail.

--- db/db.inc.php
+++ db/db.inc.php
@@ -1,35 +1,40 @@
 <?php  
-  class DatabaseAdaptor
+class DatabaseAdaptor
+{
+  protected $dbh = NULL;
+    
+  public function __construct($u = NULL, $p = NULL)
   {
-    
-    protected $dbh = NULL;
-    
-    public function __construct($u, $p)
+    if(isset($GLOBALS['DBUSER']) && isset($GLOBALS['DBPASS']))
     {
-      try {
-        $this->dbh = new PDO("mysql:host=127.0.0.1;dbname=directdb_v2", $u, $p);
-      } catch (PDOException $e)
-      {
-        print("Error connecting to the database. Please contact your system administrator.");
-        print($e);
-        die();
-      }
+      $u = $GLOBALS['DBUSER'];
+      $p = $GLOBALS['DBPASS'];
     }
-      
-    function chkAuth($u, $p)
+    
+    try {
+      $this->dbh = new PDO("mysql:host=127.0.0.1;dbname=directdb_v2", $u, $p);
+    } catch (PDOException $e)
     {
-      $p = sha1($p);
-
-      $stmt = $this->dbh->prepare("SELECT * FROM `Users` WHERE `username` = :user AND `password` = :pass");
-      $stmt->bindParam(':user', $u);
-      $stmt->bindParam(':pass', $p);
-
-      $stmt->execute();
-      
-      $rs = $stmt->fetchAll();
-      if(count($rs) == 1)
-        return true;
-        
-      return false;
+      print("Error connecting to the database. Please contact your system administrator.");
+      print($e);
+      die();
     }
   }
+    
+  function chkAuth($u, $p)
+  {
+    $p = sha1($p);
+
+    $stmt = $this->dbh->prepare("SELECT * FROM `Users` WHERE `username` = :user AND `password` = :pass");
+    $stmt->bindParam(':user', $u);
+    $stmt->bindParam(':pass', $p);
+
+    $stmt->execute();
+    
+    $rs = $stmt->fetchAll();
+    if(count($rs) == 1)
+      return true;
+      
+    return false;
+  }
+}
