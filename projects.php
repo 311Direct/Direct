d903a4c039b5889d8f387257141bb1ae3d0fc18c
James W <timeimp@icloud.com>
2015-04-26 13:53:57 +1000

    Refactored projects.php.
    Checked our REQUEST_METHOD first, then checked we were engaging in
    permissible actions.
    Created functions that use our global-scope action lists and ProjectDB
    link.
    Added a simple switch function that will take care of our requests with
    an error if we manage to pass a value that sneaks past our array and is
    not part of our program.

--- projects.php
+++ projects.php
@@ -7,27 +7,57 @@
   
   require_once 'db/json.inc.php';
   
-  if($_SERVER['REQUEST_METHOD'] == 'POST')
-  {
-    $ACTION = filter_input(INPUT_POST, 'action', FILTER_SANITIZE_STRING);
+  /* We can only use this page via POST. Die with a JSON error to inform the design team to check! */
+  if($_SERVER['REQUEST_METHOD'] != 'POST')
+    JSONResponse::printErrorResponseWithHeader("Project request specified was not valid. Error 405 - Method Not Allowed");
+  
+  /* Setup our allowable actions on this page (sans permissions) */    
+  $ACTION = filter_input(INPUT_POST, 'action', FILTER_SANITIZE_STRING);  
+  $ALLOWEDACTIONS = array('PROJECT_LIST_I_AM_MANAGING', 'PROJECT_LIST_ALL', 'PROJECT_CREATE', 'PROJECT_GET', 'PROJECT_EDIT', 'PROJECT_ATTACH_DELIVERABLE');
+  $ACTIONRESPONSE = $ACTION.'_RESPONSE';
     
-    $ALLOWEDACTIONS = array('PROJECT_LIST_I_AM_MANAGING', 'PROJECT_LIST_ALL', 'PROJECT_CREATE', 'PROJECT_GET', 'PROJECT_EDIT', 'PROJECT_ATTACH_DELIVERABLE');
-    
-    if(!in_array($ACTION, $ALLOWEDACTIONS))
-    {
+  /* Check that what we have requested to do is allowed. Return with error if we are not. */  
+  if($_SERVER['REQUEST_METHOD'] == 'POST' && !in_array($ACTION, $ALLOWEDACTIONS))
       JSONResponse::printErrorResponseWithHeader("Project request specified was not valid. Please check your variables and try again.");
-    }
-    
-    $projectsDB = new ProjectDB($DBUSER, $DBPASS);
-    $id = filter_input(INPUT_POST, 'userId', FILTER_SANITIZE_STRING);
+  
+  $projectsDB = new ProjectDB();
+  
+  /* Functions that do things to make code easier to manage */
+  function getProjectsAssignedToUser($user)
+  {
+    global $projectsDB, $ACTION, $ACTIONRESPONSE;
     $projects = $projectsDB->retrieveProjectsAssignedToUser($id);
     $projectsCount = count($projects);
-    
-    $r = new JSONResponse('PROJECT_LIST_I_AM_MANAGING_RESPONSE', array("count"=>$projectsCount, "projects"=>$projects));
+    $r = new JSONResponse($ACTIONRESPONSE, array("count"=>$projectsCount, "projects"=>$projects));
     $r->printResponseWithHeader();
-    
-  } else {
-      JSONResponse::printErrorResponseWithHeader("Project request specified was not valid. Error 405 - Method Not Allowed");    
   }
+  
+  function getAllSystemProjects($user)
+  {
+    global $projectsDB, $ACTION, $ACTIONRESPONSE;
+    $projects = $projectsDB->retrieveAllProjectInSystem($user);
+    $projectsCount = count($projects);
+    $r = new JSONResponse($ACTIONRESPONSE, array("count"=>$projectsCount, "projects"=>$projects));
+    $r->printResponseWithHeader();
+  }
+  
+  function createProject($user)
+  {
+    global $projectsDB, $ACTION, $ACTIONRESPONSE;
+  }
+  
+  $id = filter_input(INPUT_POST, 'userId', FILTER_SANITIZE_STRING);
+    
+  switch($ACTION)
+  {
+    case 'PROJECT_LIST_I_AM_MANAGING': getProjectsAssignedToUser($id);      break;
+    case 'PROJECT_LIST_ALL': getAllSystemProjects($id);       break;
+    case 'PROJECT_CREATE':      break;
+    case 'PROJECT_GET':      break;
+    case 'PROJECT_EDIT':      break;
+    case 'PROJECT_ATTACH_DELIVERABLE': break;
+    default: JSONResponse::printErrorResponseWithHeader("Project request specified was not valid. Please check your variables and try again."); break;
+  }
+    
   
 ?>
