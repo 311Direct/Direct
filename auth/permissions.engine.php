d92b6702477cb5ce36d514bf8f335f6c342ffc15
James W <timeimp@icloud.com>
2015-05-17 15:59:48 +1000

    Removed class DummyUser

--- auth/permissions.engine.php
+++ auth/permissions.engine.php
@@ -1,42 +1,13 @@
 <?php
-
-class DummyUser
-{
-  protected $projectID, $userID, $roleID;
-  
-  public function __construct($pID, $uID, $rID)
-  {
-    $this->projectID = $pID;
-    $this->userID = $uID;
-    $this->roleID = $rID;
-  }
-  
-  public function projectID()
-  {
-    return $this->projectID;
-  }
-  
-  public function userID()
-  {
-    return $this->userID;
-  }
-  
-  public function roleID()
-  {
-    return $this->roleID;
-  }
-  
-  public function __toString()
-  {
-    return 'P:'.$this->projectID.' U:'.$this->userID.' R:'.$this->roleID.'<br />';
-  }
-}
-  
+ 
 class PermissionsEngine
 {
   public function requestPermissionForOperation($projectContext, $requestingUserID, $intendedObjectID, $intendedTable, $intendedOperation)
   { 
     $rolesObject = new RolesObjectMapper($projectContext, $requestingUserID);
+    if($rolesObject->getRoleInfo() === null)
+      return false;
+      
     $permsObject = new PermissionsObjectMapper($intendedObjectID, $intendedTable, $rolesObject->getRoleInfo()->getUserID());
     
     return $this->canCompleteOperation($rolesObject, $permsObject, $intendedOperation);   
@@ -81,6 +52,7 @@ class PermissionsEngine
       }
     }
   }
+  
   public function changePermissionsForObjectWithObjects($requestingRoleObject, $intendedPermissionsObject, $intendedPermissions)
   {
     return doChangePermissions($requestingRoleObject, $intendedPermissionsObject, $intendedPermissions);
@@ -92,7 +64,6 @@ class PermissionsEngine
     $permsObject = new PermissionsObjectMapper($intendedObjectID, $intendedTable, $roleObject->getUserID());
     
     return $this->doChangePermissions($rolesObject, $permsObject, $intendedPermissions);
-     
   } 
   
   private function doChangePermissions($roleObject, $permsObject, $intendedPerms)
@@ -107,7 +78,6 @@ class PermissionsEngine
     if($intendedTable = "Role")
     {
       $rolesMapper = new RolesObjectMapper($projectContext, $requestingUserID);
-      
       $rolesMapper->updateRolePermissions($aNew);
     } 
   }
